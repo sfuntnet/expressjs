6b827cfe2a68bac9ddb69416a2618fdf590278c0
ahmetkocoglu <akocoglu@gmail.com>
2022-04-01 18:15:23 +0300

    swagger

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,10 @@ import {createConnection} from "typeorm";
 import express, { Application } from "express";
 import morgan from "morgan";
 import swaggerUi from "swagger-ui-express";
+const swaggerJsDoc = require("swagger-jsdoc");
 import Router from "./routes";
 
+const cors = require('cors')
 const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 import dbConfig from './config/database'
@@ -32,19 +34,38 @@ const io = require("socket.io")(server, {
     }
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.static("public"));
-
-app.use(
-    "/docs",
-    swaggerUi.serve,
-    swaggerUi.setup(undefined, {
-        swaggerOptions: {
-            url: "/swagger.json",
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Library API",
+            version: "1.0.0",
+            description: "A simple Express Library API",
         },
-    })
-);
+        servers: [
+            {
+                url: "http://localhost:8000",
+            },
+        ],
+    },
+    apis: ["./src/routes/*.ts"],
+};
+const specs = swaggerJsDoc(options);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+// app.use(
+//     "/docs",
+//     swaggerUi.serve,
+//     swaggerUi.setup(undefined, {
+//         swaggerOptions: {
+//             url: "/swagger.json",
+//         },
+//     })
+// );
 io.on('connection', (socket: any) => {
     console.log('a user connected');
     socket.on('abc', (veri:any) => {
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,6 +6,54 @@ import CommentRouter from "./comment.router";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Book:
+ *       type: object
+ *       required:
+ *         - title
+ *         - author
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the book
+ *         title:
+ *           type: string
+ *           description: The book title
+ *         author:
+ *           type: string
+ *           description: The book author
+ *       example:
+ *         id: d5fE_asz
+ *         title: The New Turing Omnibus
+ *         author: Alexander K. Dewdney
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Books
+ *   description: The books managing API
+ */
+
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *     summary: Returns the list of all the books
+ *     tags: [Books]
+ *     responses:
+ *       200:
+ *         description: The list of the books
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Book'
+ */
 router.get("/", async (_req, res) => {
   return res.json("start");
 });
